808e3011824c3b2c2da087613531b8f35e1c8a57
Antonio Robirosa <git@areco.e4ward.com>
2014-09-28 14:34:03 +0200

    10 Now the binaries of PMD are automatically downloaded.

--- qa/pmd-bin-5.1.3/bin/cpdgui.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set TOPDIR=%~dp0..
-set OPTS=-Xmx512m
-set MAIN_CLASS=net.sourceforge.pmd.cpd.GUI
-
-java %OPTS% -Djava.ext.dirs="%TOPDIR%\lib" %MAIN_CLASS% %*
-
--- qa/pmd-bin-5.1.3/bin/designer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -Djava.ext.dirs=..\lib net.sourceforge.pmd.util.designer.Designer
-
