14a7a09bf9373ffc9ebc8279f7de4d4b3a5056f6
Antonio Robirosa <git@areco.e4ward.com>
2015-05-13 19:53:23 +0200

    29 Now the last script is found using the pk

--- hybris/bin/custom/arecoDeploymentScriptsManager/src/org/areco/ecommerce/deploymentscripts/core/impl/FlexibleSearchScriptExecutionDao.java
+++ hybris/bin/custom/arecoDeploymentScriptsManager/src/org/areco/ecommerce/deploymentscripts/core/impl/FlexibleSearchScriptExecutionDao.java
@@ -1,28 +1,23 @@
 /**
  * Copyright 2014 Antonio Robirosa
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
 package org.areco.ecommerce.deploymentscripts.core.impl;
 
 import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
 import de.hybris.platform.servicelayer.search.FlexibleSearchService;
 import de.hybris.platform.servicelayer.search.SearchResult;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.log4j.Logger;
 import org.areco.ecommerce.deploymentscripts.core.ScriptExecutionDao;
 import org.areco.ecommerce.deploymentscripts.core.ScriptExecutionResultDAO;
@@ -32,11 +27,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Default implementatio of the dao.
- * 
+ * Default implementation of the dao.
+ *
  * @author arobirosa
- * 
  */
 @Repository
 @Scope("tenant")
@@ -82,7 +80,7 @@ public class FlexibleSearchScriptExecutionDao implements ScriptExecutionDao {
 
     /**
      * Check if the last executed deployment scripts was successful.
-     * 
+     *
      * @return true if the last deployment script was successful.
      */
     @Override
@@ -92,8 +90,9 @@ public class FlexibleSearchScriptExecutionDao implements ScriptExecutionDao {
         }
         final StringBuilder queryBuilder = new StringBuilder();
 
+        // The creation time is unreliable because on fast machines two items can have the same creation time.
         queryBuilder.append("SELECT {").append(ScriptExecutionModel.PK).append("}").append(" FROM {").append(ScriptExecutionModel._TYPECODE)
-                .append("} ORDER BY {creationtime} DESC");
+                .append("} ORDER BY {pk} DESC");
 
         final Map<String, Object> queryParams = new HashMap<String, Object>();
         if (LOG.isTraceEnabled()) {
