9a32e6975d55802e0f28094fc6281395e61c8bc5
Antonio <antonio@ecommercecandles.com>
2017-05-06 12:23:37 +0200

    41 Now the backoffice action runs the deployment scripts

---
+++ hybris/bin/custom/arecodeploymentscriptsbackoffice/backoffice/src/org/areco/ecommerce/deploymentscripts/backoffice/actions/RunPendingScriptsAction.java
@@ -0,0 +1,44 @@
+package org.areco.ecommerce.deploymentscripts.backoffice.actions;
+
+import com.hybris.cockpitng.actions.ActionContext;
+import com.hybris.cockpitng.actions.ActionResult;
+import com.hybris.cockpitng.actions.CockpitAction;
+import com.hybris.cockpitng.actions.impl.DefaultActionRenderer;
+import org.areco.ecommerce.deploymentscripts.core.DeploymentScriptStarter;
+import org.zkoss.zul.Messagebox;
+
+import javax.annotation.Resource;
+
+/**
+ * This backoffice action runs all deployment scripts which weren't run on the current database.
+ *
+ * Created by arobirosa on 28.04.17.
+ */
+public class RunPendingScriptsAction extends DefaultActionRenderer<String, String> implements CockpitAction<String, String> {
+  @Resource
+  private DeploymentScriptStarter starter;
+
+
+  @Override
+  public ActionResult<String> perform(final ActionContext<String> ctx) {
+    final ActionResult<String> result;
+    final boolean thereWasAnError = starter.runAllPendingScripts();
+    if (thereWasAnError) {
+      result = new ActionResult<>(ActionResult.ERROR, ctx.getLabel("message.therewasanerror"));
+      Messagebox.show(result.getData() + " (" + result.getResultCode() + ")");
+    } else {
+      result = new ActionResult<>(ActionResult.SUCCESS);
+    }
+    return result;
+  }
+
+  @Override
+  public boolean canPerform(final ActionContext<String> ctx) {
+    return true; // No external data is required
+  }
+
+  @Override
+  public boolean needsConfirmation(final ActionContext<String> ctx) {
+    return false;
+  }
+}
