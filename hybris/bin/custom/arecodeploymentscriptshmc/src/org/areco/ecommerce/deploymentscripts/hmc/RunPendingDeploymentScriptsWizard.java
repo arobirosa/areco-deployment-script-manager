91f4dd99898b193cded44398b5e80f774a3c59f0
Antonio <antonio@ecommercecandles.com>
2017-05-06 17:06:57 +0200

    41 The hmc code and config was moved to the new extension

---
+++ hybris/bin/custom/arecodeploymentscriptshmc/src/org/areco/ecommerce/deploymentscripts/hmc/RunPendingDeploymentScriptsWizard.java
@@ -0,0 +1,49 @@
+package org.areco.ecommerce.deploymentscripts.hmc;
+
+import de.hybris.platform.core.Registry;
+import de.hybris.platform.hmc.jalo.VetoException;
+import de.hybris.platform.hmc.jalo.WizardEditorContext;
+import de.hybris.platform.util.localization.Localization;
+
+import org.apache.log4j.Logger;
+import org.areco.ecommerce.deploymentscripts.core.DeploymentScriptStarter;
+
+/**
+ * This hmc wizard runs all the deployment scripts which weren't executed yet.
+ *
+ * @author arobirosa
+ *
+ */
+public class RunPendingDeploymentScriptsWizard extends GeneratedRunPendingDeploymentScriptsWizard {
+	private static final Logger LOG = Logger.getLogger(RunPendingDeploymentScriptsWizard.class);
+
+	/*
+   * { @InheritDoc }
+   */
+	@Override
+	public void start(final WizardEditorContext ctx) throws VetoException {
+		super.start(ctx);
+		if (LOG.isInfoEnabled()) {
+			LOG.info("Starting wizard.");
+		}
+		ctx.hideButton(NEXT_BUTTON);
+		ctx.hideButton(BACK_BUTTON);
+		final DeploymentScriptStarter starter = Registry.getApplicationContext().getBean(DeploymentScriptStarter.class);
+		final boolean thereWasAnError = starter.runAllPendingScripts();
+		if (thereWasAnError) {
+			ctx.showErrorTab(Localization.getLocalizedString("runpendingdeploymentscriptswizard.message.therewasanerror"));
+		} else {
+			ctx.showSummaryTab(Localization.getLocalizedString("runpendingdeploymentscriptswizard.message.noerrors"));
+		}
+	}
+
+	/*
+   * { @InheritDoc }
+   */
+	@Override
+	public void initialize(final WizardEditorContext ctx) {
+		super.initialize(ctx);
+		ctx.enableButton(START_BUTTON);
+	}
+
+}
