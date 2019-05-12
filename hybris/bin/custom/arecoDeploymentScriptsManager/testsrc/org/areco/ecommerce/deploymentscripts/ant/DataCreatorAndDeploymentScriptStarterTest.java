60c52aed95d37a837e1170f669b07645f7080c19
„Antonio <antonio@ecommercecandles.com>
2019-05-12 18:56:12 +0200

    53 Now the order of the init and update scripts is tested

--- hybris/bin/custom/arecoDeploymentScriptsManager/testsrc/org/areco/ecommerce/deploymentscripts/ant/DataCreatorAndDeploymentScriptStarterTest.java
+++ hybris/bin/custom/arecoDeploymentScriptsManager/testsrc/org/areco/ecommerce/deploymentscripts/ant/DataCreatorAndDeploymentScriptStarterTest.java
@@ -15,18 +15,21 @@
  */
 package org.areco.ecommerce.deploymentscripts.ant;
 
+import static org.areco.ecommerce.deploymentscripts.constants.ArecoDeploymentScriptsManagerConstants.*;
 import de.hybris.bootstrap.annotations.IntegrationTest;
-import de.hybris.platform.core.Registry;
+import de.hybris.platform.core.model.order.price.TaxModel;
+import de.hybris.platform.order.daos.TaxDao;
 import junit.framework.Assert;
-import org.apache.log4j.Logger;
 import org.areco.ecommerce.deploymentscripts.core.AbstractWithConfigurationRestorationTest;
 import org.areco.ecommerce.deploymentscripts.core.TenantDetector;
-import org.areco.ecommerce.deploymentscripts.core.impl.RegistryTenantDetector;
 import org.areco.ecommerce.deploymentscripts.testhelper.DeploymentScriptResultAsserter;
-import org.fest.util.Collections;
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.util.List;
+
 
 /**
  * It checks if the essentialDataCreator triggers the essential data creation and runs the scripts. Hybris cannot change the tenant when we are inside a
@@ -38,9 +41,9 @@ import javax.annotation.Resource;
 @IntegrationTest
 public class DataCreatorAndDeploymentScriptStarterTest extends AbstractWithConfigurationRestorationTest {
 
-    private static final String RESOURCES_FOLDER = "/resources/test/essential-data-creator";
+    private static final String CREATOR_RESOURCES_FOLDER = "/resources/test/essential-data-creator";
 
-    private static final Logger LOG = Logger.getLogger(DataCreatorAndDeploymentScriptStarterTest.class);
+    private static final String ORDER_TEST_RESOURCES_FOLDER = "/resources/test/run-order";
 
     @Resource
     private DeploymentScriptResultAsserter deploymentScriptResultAsserter;
@@ -57,21 +60,38 @@ public class DataCreatorAndDeploymentScriptStarterTest extends AbstractWithConfi
     @Resource
     private TenantDetector registryTenantDetector;
 
-    @Test
-    public void testRunScripts() {
-        // Hybris cannot change the tenant when we are inside a transaction, because we need transactions -import the essential data-, this test only works in
-        // single tenant environments.
-        if (!registryTenantDetector.areWeInATestSystemWithOneSingleTenant()) {
-            this.getDeploymentConfigurationSetter().setTestFolders(RESOURCES_FOLDER, "update-scripts", "init-scripts");
-            this.dataCreatorAndDeploymentScriptsStarter.runInJunitTenant();
-            Assert.assertTrue("There were errors running the deployment scripts", antDeploymentScriptsStarter.wasLastScriptSuccessful());
-            deploymentScriptResultAsserter.assertSuccessfulResult("20141005_RELOAD_CMS_CONF");
-            deploymentScriptResultAsserter.assertSuccessfulResult("20141004_RELOAD_CMS_CONF");
-            Assert.assertTrue("The creation of the essential data wasn't triggered.", essentialDataCreationDetector.isWasEssentialDataCreated());
-            Assert.assertTrue("The creation of the project data wasn't triggered.", essentialDataCreationDetector.isWasProjectDataCreated());
-        } else {
-            LOG.error("Ignoring this test because it only has one tenant.");
-        }
+    @Resource
+    private TaxDao taxDao;
+
+    @Before
+    public void skipIfJunitTenantDoesNotExist() {
+        Assume.assumeFalse("Hybris cannot change the tenant when we are inside a transaction, because we need transactions to import the essential data, this test only works in environments with a separated junit tenant", registryTenantDetector.areWeInATestSystemWithOneSingleTenant());
     }
+
+
+    @Test
+    public void testRunDataCreationSteps() {
+        this.getDeploymentConfigurationSetter().setTestFolders(CREATOR_RESOURCES_FOLDER, DEFAULT_UPDATE_SCRIPTS_FOLDER, DEFAULT_INIT_SCRIPTS_FOLDER);
+        this.dataCreatorAndDeploymentScriptsStarter.runInJunitTenant();
+        Assert.assertTrue("There were errors running the deployment scripts", antDeploymentScriptsStarter.wasLastScriptSuccessful());
+        deploymentScriptResultAsserter.assertSuccessfulResult("20141005_RELOAD_CMS_CONF");
+        deploymentScriptResultAsserter.assertSuccessfulResult("20141004_RELOAD_CMS_CONF");
+        Assert.assertTrue("The creation of the essential data wasn't triggered.", essentialDataCreationDetector.isWasEssentialDataCreated());
+        Assert.assertTrue("The creation of the project data wasn't triggered.", essentialDataCreationDetector.isWasProjectDataCreated());
+    }
+
+    @Test
+    public void testInitAndUpdateScriptsOrder() {
+        this.getDeploymentConfigurationSetter().setTestFolders(ORDER_TEST_RESOURCES_FOLDER, DEFAULT_UPDATE_SCRIPTS_FOLDER, DEFAULT_INIT_SCRIPTS_FOLDER);
+        this.dataCreatorAndDeploymentScriptsStarter.runInJunitTenant();
+        Assert.assertTrue("The update script was run before the init script", antDeploymentScriptsStarter.wasLastScriptSuccessful());
+        deploymentScriptResultAsserter.assertSuccessfulResult("20190512_INSERT_TAX");
+        deploymentScriptResultAsserter.assertSuccessfulResult("20190512_UPDATE_TAX");
+        List<TaxModel> foundTaxes = taxDao.findTaxesByCode("dummyRunOrderTax");
+        Assert.assertFalse("The test tax don't exist", foundTaxes.isEmpty());
+        Assert.assertEquals("Many test taxes exist", 1, foundTaxes.size());
+        Assert.assertEquals("The tax wasn't updated", 19d, foundTaxes.get(0).getValue());
+    }
+
 
 }
