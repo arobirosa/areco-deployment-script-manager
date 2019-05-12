cb0d402d21c2adbee1ca2a6fcd2e5da1dac0d664
„Antonio <antonio@ecommercecandles.com>
2019-05-12 18:16:06 +0200

    53 Test to show that the init scripts aren't called

--- hybris/bin/custom/arecoDeploymentScriptsManager/testsrc/org/areco/ecommerce/deploymentscripts/ant/DataCreatorAndDeploymentScriptStarterTest.java
+++ hybris/bin/custom/arecoDeploymentScriptsManager/testsrc/org/areco/ecommerce/deploymentscripts/ant/DataCreatorAndDeploymentScriptStarterTest.java
@@ -16,10 +16,14 @@
 package org.areco.ecommerce.deploymentscripts.ant;
 
 import de.hybris.bootstrap.annotations.IntegrationTest;
+import de.hybris.platform.core.Registry;
 import junit.framework.Assert;
+import org.apache.log4j.Logger;
 import org.areco.ecommerce.deploymentscripts.core.AbstractWithConfigurationRestorationTest;
 import org.areco.ecommerce.deploymentscripts.core.TenantDetector;
+import org.areco.ecommerce.deploymentscripts.core.impl.RegistryTenantDetector;
 import org.areco.ecommerce.deploymentscripts.testhelper.DeploymentScriptResultAsserter;
+import org.fest.util.Collections;
 import org.junit.Test;
 
 import javax.annotation.Resource;
@@ -35,6 +39,8 @@ import javax.annotation.Resource;
 public class DataCreatorAndDeploymentScriptStarterTest extends AbstractWithConfigurationRestorationTest {
 
     private static final String RESOURCES_FOLDER = "/resources/test/essential-data-creator";
+
+    private static final Logger LOG = Logger.getLogger(DataCreatorAndDeploymentScriptStarterTest.class);
 
     @Resource
     private DeploymentScriptResultAsserter deploymentScriptResultAsserter;
@@ -55,13 +61,16 @@ public class DataCreatorAndDeploymentScriptStarterTest extends AbstractWithConfi
     public void testRunScripts() {
         // Hybris cannot change the tenant when we are inside a transaction, because we need transactions -import the essential data-, this test only works in
         // single tenant environments.
-        if (registryTenantDetector.areWeInATestSystemWithOneSingleTenant()) {
-            this.getDeploymentConfigurationSetter().setTestFolders(RESOURCES_FOLDER, "scripts");
+        if (!registryTenantDetector.areWeInATestSystemWithOneSingleTenant()) {
+            this.getDeploymentConfigurationSetter().setTestFolders(RESOURCES_FOLDER, "update-scripts", "init-scripts");
             this.dataCreatorAndDeploymentScriptsStarter.runInJunitTenant();
             Assert.assertTrue("There were errors running the deployment scripts", antDeploymentScriptsStarter.wasLastScriptSuccessful());
+            deploymentScriptResultAsserter.assertSuccessfulResult("20141005_RELOAD_CMS_CONF");
             deploymentScriptResultAsserter.assertSuccessfulResult("20141004_RELOAD_CMS_CONF");
             Assert.assertTrue("The creation of the essential data wasn't triggered.", essentialDataCreationDetector.isWasEssentialDataCreated());
             Assert.assertTrue("The creation of the project data wasn't triggered.", essentialDataCreationDetector.isWasProjectDataCreated());
+        } else {
+            LOG.error("Ignoring this test because it only has one tenant.");
         }
     }
 
