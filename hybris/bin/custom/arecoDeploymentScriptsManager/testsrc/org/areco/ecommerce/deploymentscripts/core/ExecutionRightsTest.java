e2301ffc5b70479e7adbaedaf717b3d7ec4d5ab1
Antonio <antonio@ecommercecandles.com>
2019-08-07 12:26:14 +0200

    54 PMD Violation

--- hybris/bin/custom/arecoDeploymentScriptsManager/testsrc/org/areco/ecommerce/deploymentscripts/core/ExecutionRightsTest.java
+++ hybris/bin/custom/arecoDeploymentScriptsManager/testsrc/org/areco/ecommerce/deploymentscripts/core/ExecutionRightsTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import javax.annotation.Resource;
 
 @IntegrationTest
+@SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert") // The assert statements are inside a private method.
 public class ExecutionRightsTest extends AbstractWithConfigurationRestorationTest {
 
         private static final String RESOURCES_FOLDER = "/resources/test/execution-rights";
