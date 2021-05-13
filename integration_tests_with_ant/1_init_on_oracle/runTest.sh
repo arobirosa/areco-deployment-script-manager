f36cf9b26b56523058b8e4c653aefb7adf352958
Antonio <antonio@ecommercecandles.com>
2021-05-13 14:09:21 +0200

    15 Now hybris can connect to the oracle database. Other method to wait for the database, not working

--- integration_tests_with_ant/1_init_on_oracle/runTest.sh
+++ integration_tests_with_ant/1_init_on_oracle/runTest.sh
@@ -18,7 +18,13 @@ export COMPOSE_TLS_VERSION=TLSv1_2;
 [[ -f $ARECO_HYBRIS_DIR/bin/platform/hybrisserver.sh ]] || (echo "Please configure ARECO_HYBRIS_DIR with the directory where SAP commerce is located." && exit 1);
 [[ -d $ARECO_DB_DATA_FOLDER ]] || (echo "I can't found the shared directory with the database data" && exit 2);
 
-cp -v $ARECO_CURRENT_TEST_FOLDER/../ojdbc6.jar $ARECO_HYBRIS_DIR/bin/platform/lib/dbdriver/;
+cp -v $ARECO_CURRENT_TEST_FOLDER/../dbdriver/*.jar $ARECO_HYBRIS_DIR/bin/platform/lib/dbdriver/;
+
+echo "Starting the oracle XE container"
+docker-compose --file $ARECO_CURRENT_TEST_FOLDER/docker-compose.yml up -d;
+$ARECO_CURRENT_TEST_FOLDER/../utils/wait-for-it.sh --host=127.0.0.1 --port=9500 --timeout=600 -- echo "Waiting for the oracle database to be ready";
+docker-compose --file $ARECO_CURRENT_TEST_FOLDER/docker-compose.yml logs --follow areco-database | grep 'DATABASE IS READY TO USE' | read -t 600 areco_unused_variable
+[ $? -eq 0 ]  && echo 'Oracle XE is up'  || (echo 'Oracle XE did not start' && exit 3)
 
 echo "Configuring database connection and other properties";
 export HYBRIS_OPT_CONFIG_DIR=$ARECO_CURRENT_TEST_FOLDER;
@@ -27,20 +33,10 @@ cd ../..;
 . ./setantenv.sh;
 echo "START TEST";
 
-echo "Clearing the database and data folder"
+echo "Dropping and recreating the user and cleaning data folder"
 rm -rf $ARECO_HYBRIS_DIR/data/*;
-docker-compose --file $ARECO_CURRENT_TEST_FOLDER/docker-compose.yml down;
-# rm -rf $ARECO_DB_DATA_FOLDER/*;
-docker-compose --file $ARECO_CURRENT_TEST_FOLDER/docker-compose.yml up -d;
-$ARECO_CURRENT_TEST_FOLDER/../utils/wait-for-it.sh --host=127.0.0.1 --port=9500 --timeout=600 -- echo "Waiting for the oracle database to be ready";
 
 echo "Run all the tests on master tenant"
 ant clean all yunitinit qa;
-
-## Clear the database and data folder
-#rm -rf $ARECO_HYBRIS_DIR/data;
-
-# Run all the tests with on junit tenant:
-#ant clean all initialize yunitinit qa
 
 echo "TEST SUCCESS"
