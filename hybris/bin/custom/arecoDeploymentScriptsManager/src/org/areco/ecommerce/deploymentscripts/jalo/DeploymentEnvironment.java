9294ccdd432573d6b2517fb005b20b88621e885f
Antonio Robirosa <antonio@areko.consulting>
2023-11-01 17:41:07 +0200

    32 Removal of the jalo classes

--- hybris/bin/custom/arecoDeploymentScriptsManager/src/org/areco/ecommerce/deploymentscripts/jalo/DeploymentEnvironment.java
+++
@@ -1,9 +0,0 @@
-package org.areco.ecommerce.deploymentscripts.jalo;
-
-/**
- * Represents a group of servers where the deployment scripts may run.
- */
-public class DeploymentEnvironment extends GeneratedDeploymentEnvironment {
-    // We don't have any jalo method.
-
-}
